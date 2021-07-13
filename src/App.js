117ed9716b89ecd533374ab85fd14f2ffb5fb59c
skullKim <space1878@naver.com>
2021-07-13 14:57:15 +0900

    추가: 책 리스트 보여주기 완료

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import BookLists from "./components/BookLists";
+import styled from "styled-components";
+
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Main className="App">
       <BookLists/>
-    </div>
+    </Main>
   );
 }
 
--- src/components/BookLists.js
+++ src/components/BookLists.js
@@ -1,13 +1,35 @@
 import React, {useState, useEffect} from 'react';
+import styled from 'styled-components';
 import axios from "axios";
 
+
+const BooksInformation = styled.div`
+  height: 300px;
+  width: 700px;
+  border: 2px solid black;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  margin-top: 10px;
+`;
+
+const BookInfo = styled.div`
+  display: flex;
+  margin: 0 20px;
+`;
+
+const BookLink = styled('a')`
+  text-decoration: none;
+  color: black;
+`;
+
 const BookLists = () => {
-    const [books, setBooks] = useState([]);
+    const [books, setBooks] = useState(null);
     const [loading, setLoading] = useState(false);
     useEffect(() => {
         const booksData = async() => {
+            setLoading(true);
             try {
-                setLoading(true);
                 const {data: {documents}} = await axios({
                     method: 'get',
                     url: 'https://dapi.kakao.com/v3/search/book?target=title',
@@ -16,8 +38,8 @@ const BookLists = () => {
                         query: '프로그래밍',
                     }
                 });
-                setLoading(false);
                 setBooks(documents);
+                setLoading(false);
             }
             catch(err) {
                return console.error(err);
@@ -30,20 +52,27 @@ const BookLists = () => {
         return(<div>Loading...</div>);
     }
 
+    if(!books) {
+        return null;
+    }
 
-
+     console.log(books);
     return (
         <div>
-            {books.map(({authors, contents, thumbnail, title}) => (
-                <div>
-                    <div>{authors}</div>
-                    <div>{contents}</div>
-                    <img src={thumbnail} alt={"thumbnail"}/>
-                    <div>{title}</div>
-                </div>
+            {books.map(({authors, contents, thumbnail, title, url}) => (
+                <BooksInformation>
+                    <BookInfo>
+                        <p><BookLink href={url}>제목: {title},</BookLink></p>
+                        <p>저자: {authors}</p>
+                    </BookInfo>
+                    <BookInfo>
+                        <img src={thumbnail} alt="thumbnail"/>
+                        <p>{contents}</p>
+                    </BookInfo>
+                </BooksInformation>
             ))}
         </div>
     );
-};
+}
 
 export default BookLists;
